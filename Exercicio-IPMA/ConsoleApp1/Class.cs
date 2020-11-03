0f40e785de53ba9a98dd79a6bf3ae1b21048fe9b
PJ16979 <65820758+PJ16979@users.noreply.github.com>
2020-11-03 17:43:05 +0000

    Exercicios

---
+++ Exercicio-IPMA/ConsoleApp1/Class.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class PrevisaoDia
+    {
+        public string precipitaProb { get; set; }
+        public string tMin { get; set; }
+        public string tMax { get; set; }
+        public string predWindDir { get; set; }
+        public int idWeatherType { get; set; }
+        public int classWindSpeed { get; set; }
+        public string longitude { get; set; }
+        public string forecastDate { get; set; }
+        public int classPrecInt { get; set; }
+        public string latitude { get; set; }
+    }
+
+    public class PrevisaoIPMA
+    {
+        public string owner { get; set; }
+        public string country { get; set; }
+        public List<PrevisaoDia> data { get; set; }
+        public int globalIdLocal { get; set; }
+        public DateTime dataUpdate { get; set; }
+        public string Local { get; set; }
+    }
+}
---
+++ Exercicio-IPMA/ConsoleApp1/Program.cs
@@ -0,0 +1,97 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static Dictionary<int, string> LerLocais(string ficheiro)
+        {
+
+            Dictionary<int, string> dicLocais = new Dictionary<int, string>();
+
+            // Expressão Regular para instanciar objeto Regex
+            String erString = @"^[0-9]{7},[123],([1-9]?\d,){2}[A-Z]{3},([^,\n]*)$";
+            Regex g = new Regex(erString);
+
+            using (StreamReader r = new StreamReader(ficheiro))
+            {
+                string line;
+
+                while ((line = r.ReadLine()) != null)
+                {
+                    Match m = g.Match(line);
+                    if (m.Success)
+                    {
+                        string[] campos = m.Value.Split(',');
+                        int codLocal = int.Parse(campos[0]);
+                        string cidade = campos[5];
+                        dicLocais.Add(codLocal, cidade);
+                        continue;
+                    }
+                }
+            }
+
+            return dicLocais;
+        }
+
+        static PrevisaoIPMA LerFicheiroPrevisao(int globalID)
+        {
+            String jsonString = null;
+
+            using (StreamReader reader = new StreamReader(@"../data_forecast/" + globalID + ".json"))
+                jsonString = reader.ReadToEnd();
+
+            PrevisaoIPMA obj = System.Text.Json.JsonSerializer.Deserialize<PrevisaoIPMA>(jsonString);
+            return obj;
+        }
+
+        static void EscreveOutput(PrevisaoIPMA leitura)
+        {
+            string jsonData = JsonConvert.SerializeObject(leitura);
+            File.WriteAllText(@"../outputs/" + leitura.globalIdLocal + ".json", jsonData);
+
+            XmlDocument xmlDocument = JsonConvert.DeserializeXmlNode(jsonData, "root");
+            xmlDocument.Save(@"../outputs/" + leitura.globalIdLocal + ".xml");
+        }
+
+        static void Main(string[] args)
+        {
+            PrevisaoIPMA leitura;
+            Dictionary<int, string> locais = LerLocais("../locais.csv");
+            int idGlobal;
+
+            foreach (string ficheiro in Directory.GetFiles("../data_forecast/"))
+            {
+                idGlobal = 0;
+                int.TryParse(Path.GetFileNameWithoutExtension(ficheiro), out idGlobal);
+
+                // Verificar se a conversão foi feita corretamente. 
+                // Caso não tenha sido, passa para o próximo ficheiro
+                if (idGlobal == 0) continue;
+
+                leitura = LerFicheiroPrevisao(idGlobal);
+
+                try { leitura.Local = locais[leitura.globalIdLocal]; }
+                catch { continue; }
+
+                EscreveOutput(leitura);
+
+                foreach (PrevisaoDia registo in leitura.data)
+                    Console.WriteLine($"Dia: {registo.forecastDate} | Minímo: {registo.tMin} | Máximo: {registo.tMax} | Prob. precipitação: {registo.precipitaProb}\n");
+            }
+            
+
+
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
